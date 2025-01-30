1d86299aac0e34835eba843169f4461c373482fb
Huzaifa-Akram <huzaifaakram121@gmail.com>
2025-01-31 04:16:26 +0500

    3

--- Nokhba/Default.aspx
+++ Nokhba/Default.aspx
@@ -1,5 +1,21 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Nokhba.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+        <link rel="stylesheet" href="StyleSheet1.css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="page-title">
+        <h1>Want To Find The Best Jobs?</h1>
+        <p style="font-size:1.2rem;">You're at the right place</p>
+    </div>
+    <div class="search-bar-outer-container">
+        <div class="search-bar-inner-container">
+            <input type="text" class="job-name-search" placeholder="Enter Job Title" />
+            <div class="pipe">|</div>
+            <asp:DropDownList CssClass="experience-search" ID="experienceDropDown" runat="server" EnableViewState="true"></asp:DropDownList>
+            <div class="pipe">|</div>
+            <input type="text" class="job-location-search" placeholder="Enter a City" />
+            <asp:Button runat="server" Text="Search" CssClass="job-search-btn" />
+        </div>
+    </div>
+
 </asp:Content>
--- Nokhba/Default.aspx.cs
+++ Nokhba/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,33 @@ namespace Nokhba
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Fill_Experience_DropDown();
+            }
 
         }
-    }
+        protected void Fill_Experience_DropDown()
+        {
+            DataSet ds = new DataSet();
+            DataTable dtFields = new DataTable();
+            dtFields.Columns.Add("Years", typeof(int));
+            dtFields.Columns.Add("Experience", typeof(string));
+
+            dtFields.Rows.Add(0, "Fresher");
+
+            for (int i = 0; i < 30; i++)
+            {
+                dtFields.Rows.Add(i + 1, $"{i + 1} Years");
+            }
+
+            System.Diagnostics.Debug.WriteLine($"Rows in dtFields: {dtFields.Rows.Count}");
+
+
+            experienceDropDown.DataSource = dtFields;
+            experienceDropDown.DataTextField = "Experience";
+            experienceDropDown.DataValueField = "Years";
+            experienceDropDown.DataBind();
+        }
+}
 }
